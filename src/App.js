fbbb2fa24b7b56423bbf4056b365db4407246c9f
jahedripon9 <jahedahmed3170@gmail.com>
2023-01-07 11:06:52 +0600

    News List Section finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import "./App.css";
 import React from "react";
 import Header from "./Pages/header";
 import { newsCategory } from "./news";
+import NewsList from "./Pages/newsList/newsList";
+
+const fakeNews = [
+  {
+    title: "Title",
+    content: "Content",
+    url: "https://linktonews.com",
+    urlToImage: "https://linktoimage.com",
+    publishedAt: 'published date and time',
+    source : {
+      name: "CNN"
+    }, 
+  },
+  {
+    title: "Title",
+    content: "Content",
+    url: "https://linktonews.com",
+    urlToImage: "https://linktoimage.com",
+    publishedAt: 'published date and time',
+    source : {
+      name: "CNN"
+    }, 
+  },
+]
+
 
 class App extends React.Component {
   render() {
@@ -10,6 +35,7 @@ class App extends React.Component {
         <div className="row">
           <div className="col-sm-6 offset-md-3">
             <Header category={newsCategory.technology} />
+            <NewsList news={fakeNews} />
           </div>
         </div>
       </div>
