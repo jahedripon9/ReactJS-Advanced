5977a4fb4a6497eae560e6fb8f93cc72eee6c83d
jahedripon9 <jahedahmed3170@gmail.com>
2023-01-07 10:16:41 +0600

    first commit

---
+++ src/Pages/header.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { newsCategory } from '../news'
+
+class Header extends Component {
+    state = {
+        searchTerm: ''
+    }
+
+    handleChange = e => {
+      this.setState({searchTerm: e.target.value})
+    }
+
+    handleKeyPress= e => {
+        
+    }
+  render() {
+    const {category} = this.props
+    return (
+      <div className='my-4'>
+        <h1 className='mb-4' style={{fontWeight: '300'}}>
+            Block Buster Headlines  
+        </h1>
+        <input type="search" className="form-control"
+            placeholder="Type Anything & Press Enter to Search "
+            value={this.state.searchTerm} 
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            />
+            <div className='my-4'>
+              {newsCategory && Object.keys(newsCategory).map(item => {
+                if (category === newsCategory[item]){
+                    return(
+                      <button className='btn btn-sm btn-warning mr-2 mb-2'> 
+                        {`#${newsCategory[item]}`}
+                      </button>
+                    )
+                      }
+                      return(
+                        <button className='btn btn-sm btn-light mr-2 mb-2'>
+                            {`#${newsCategory[item]}`}
+                        </button>
+                      )
+                    })}
+            </div>
+      </div>
+    )
+  }
+}
+
+export default Header
